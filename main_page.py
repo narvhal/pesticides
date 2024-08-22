b3c1dca92b4b9053aa6398d0d449e21535f6e6d1
Nari Miller <narvhal@users.noreply.github.com>
2024-08-22 15:49:05 -0700

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,7 +1,9 @@
 import folium
 import streamlit as st
-
 from streamlit_folium import st_folium
+import pandas as pd
+import geopandas
+import requests
 
 # center on Liberty Bell, add marker
 m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
@@ -15,3 +17,16 @@ st_data = st_folium(m, width=725)
 
 
 ### geojson map like: https://folium.streamlit.app/geojson_popup, https://python-visualization.github.io/folium/latest/user_guide/geojson/geojson_popup_and_tooltip.html
+#GeoJSON popup and tooltip
+income = pd.read_csv(
+    "https://raw.githubusercontent.com/pri-data/50-states/master/data/income-counties-states-national.csv",
+    dtype={"fips": str})
+income["income-2015"] = pd.to_numeric(income["income-2015"], errors="coerce")
+
+
+data = requests.get(
+    "https://raw.githubusercontent.com/python-visualization/folium-example-data/main/us_states.json"
+).json()
+states = geopandas.GeoDataFrame.from_features(data, crs="EPSG:4326")
+
+states.head()
