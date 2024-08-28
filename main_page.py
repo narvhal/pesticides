083bbf753e8d4b1f7e3ca3a75e40996f18cd21bc
Nari M <nari.v.miller@gmail.com>
2024-08-27 17:30:40 -0700

    sm edits

--- main_page.py
+++ main_page.py
@@ -14,16 +14,27 @@ import fiona
 st.set_page_config(layout="wide" )
 
 
-permittee, site_id, permit_num, permit_yr, loc_narr, is_active, size =load_standard_colnames()
+toc = stoc()
+
+# Title
+st.title("Stanislaus County Pesticide Use Reports and School Locations")
+
+
+st.header("1. Select filters")
+
+
+
+
 ##########################
+permittee, site_id, permit_num, permit_yr, loc_narr, is_active, size =load_standard_colnames()
 
 df, fbs = prepare_df_from_stanag()
 
 # if st.button("Show filter options"):
 #     print_col_uniques(df)
 
-selcol = st.empty()
-selcolval = st.empty()
+selcol = None
+selcolval = None
 with st.popover("Select filter"):
     for i,d in enumerate(df.columns.to_list()):
         selcol = st.checkbox(str(d), key = "selcol_chkbox_" + str(i))
@@ -35,17 +46,14 @@ with st.popover("Select filter"):
 
 if selcol in df.columns.to_list():
     if selcolval in df[selcol].unique():
+
         dfn = filt_df(df, selcol, selcolval, type_compare="==")
         add_geometry2(dfn, fbs)
 
 #################
-toc = stoc()
-
-# Title
-st.title("Stanislaus County Pesticide Use Reports and School Locations")
 
 
-st.header("1. Select filters")
+st.header("2. Map")
 # center on Liberty Bell, add marker
 m = folium.Map(location=[37.5, -120.8], zoom_start=5)
 
@@ -66,21 +74,21 @@ dfc = None
 colorcol = None
 with st.popover("Select column to colorize map"):
     st.write("PUR data: ")
-    for d in df.columns.to_list():
-        colorcol = st.checkbox(str(d))
+    for i, d in enumerate(df.columns.to_list()):
+        colorcol = st.checkbox(str(d), key = "colorcol_chkbox1_" + str(i))
         dfc = df.copy()
-    st.write("Field Bounds Data: ")
-    for d in fbs.columns.to_list():
-        colorcol = st.checkbox(str(d))
-        dfc = fbs.copy()
-    st.write("Private Schools Data: ")
-    for d in spriv.columns.to_list():
-        colorcol = st.checkbox(str(d))
-        dfc = spriv.copy()
-    st.write("Public Schools Data: ")
-    for d in spub.columns.to_list():
-        colorcol = st.checkbox(str(d))
-        dfc = spub.copy()
+    # st.write("Field Bounds Data: ")
+    # for i, d in enumerate(fbs.columns.to_list()):
+    #     colorcol = st.checkbox(str(d), key = "colorcol_chkbox2_" + str(i))
+    #     dfc = fbs.copy()
+    # st.write("Private Schools Data: ")
+    # for  i, d in enumerate(spriv.columns.to_list()):
+    #     colorcol = st.checkbox(str(d), key = "colorcol_chkbox3_" + str(i))
+    #     dfc = spriv.copy()
+    # st.write("Public Schools Data: ")
+    # for  i, d in enumerate(spub.columns.to_list()):
+    #     colorcol = st.checkbox(str(d), key = "colorcol_chkbox4_" + str(i))
+    #     dfc = spub.copy()
 
 
 
@@ -90,8 +98,8 @@ if dfc is not None:
 
 
     colormap = branca.colormap.LinearColormap(
-        vmin=pur[colorcol].quantile(0.0),
-        vmax=pur[colorcol].quantile(1),
+        vmin=dfc[colorcol].quantile(0.0),
+        vmax=dfc[colorcol].quantile(1),
         colors=["red", "orange", "lightblue", "green", "darkgreen"],
         caption=colorcol_desc,
     )
@@ -99,16 +107,16 @@ if dfc is not None:
     m = folium.Map(location=[35.3, -97.6], zoom_start=4)
 
     popup = folium.GeoJsonPopup(
-        fields=["name", colorcol],
-        aliases=["State", "% Change"],
+        fields=["Site Location", colorcol],
+        aliases=["Site Location", colorcol_desc],
         localize=True,
         labels=True,
         style="background-color: yellow;",
     )
 
     tooltip = folium.GeoJsonTooltip(
-        fields=["name", "medianincome", colorcol],
-        aliases=["State:", "2015 Median Income(USD):", "Median % Change:"],
+        fields=["Site Location", "Application Date", colorcol],
+        aliases=["Site Location", "Application Date", colorcol_desc],
         localize=True,
         sticky=False,
         labels=True,
