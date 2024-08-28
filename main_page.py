b8fb5be851abcc04e1694824956da52f89cdee73
Nari M <nari.v.miller@gmail.com>
2024-08-28 14:26:07 -0700

    add plt map plot method

--- main_page.py
+++ main_page.py
@@ -61,7 +61,7 @@ if selection_type == "Chemical Type":
     key = "multisel_products"
     with mc:
         def get_new_values_list():
-            st.write(st.session_state["multisel_products"])
+            st.session_state["multisel_products"]
         cname = st.multiselect("Select Products", ccnames, on_change = get_new_values_list, key = key )
 
     colorcol = "Product Name"
@@ -163,8 +163,6 @@ if flag_keep_going:
 
 # center on Liberty Bell, add marker
 
-# st.write(dfc[[colorcol, "color_category", "color"]])
-
     if st.checkbox("Plot map", key = "plotmapcheckbx"):
         st.header("Example Map: Aerial applications")
         with rc:
@@ -205,30 +203,30 @@ if flag_keep_going:
             buffer_alphas = [ 0.5, 0.5]  # Buffer transparency
             buffer_legend_flags = [True, False]  # Whether to include in legend
 
-            fig  = plot_geopandas_with_legend(
-                polygon_dfs=polygon_dfs,
-                polygon_colors=polygon_colors,
-                polygon_labels=polygon_labels,
-                polygon_alphas=polygon_alphas,
-                polygon_legend_flags=polygon_legend_flags,
-                point_dfs=point_dfs,
-                point_markers=point_markers,
-                point_colors=point_colors,
-                point_sizes=point_sizes,
-                point_labels=point_labels,
-                point_legend_flags=point_legend_flags,
-                categorized_dfs=categorized_dfs,
-                category_columns=category_columns,
-                category_colors_list=category_colors_list,
-                category_legend_flags=category_legend_flags,
-                buffer_dfs=buffer_dfs,
-                buffer_colors=buffer_colors,
-                buffer_labels=buffer_labels,
-                buffer_alphas=buffer_alphas,
-                buffer_legend_flags=buffer_legend_flags,
-                title=f'Fields within {size} miles of school',
-                figsize=(10, 16)
-            )
+            # fig  = plot_geopandas_with_legend(
+            #     polygon_dfs=polygon_dfs,
+            #     polygon_colors=polygon_colors,
+            #     polygon_labels=polygon_labels,
+            #     polygon_alphas=polygon_alphas,
+            #     polygon_legend_flags=polygon_legend_flags,
+            #     point_dfs=point_dfs,
+            #     point_markers=point_markers,
+            #     point_colors=point_colors,
+            #     point_sizes=point_sizes,
+            #     point_labels=point_labels,
+            #     point_legend_flags=point_legend_flags,
+            #     categorized_dfs=categorized_dfs,
+            #     category_columns=category_columns,
+            #     category_colors_list=category_colors_list,
+            #     category_legend_flags=category_legend_flags,
+            #     buffer_dfs=buffer_dfs,
+            #     buffer_colors=buffer_colors,
+            #     buffer_labels=buffer_labels,
+            #     buffer_alphas=buffer_alphas,
+            #     buffer_legend_flags=buffer_legend_flags,
+            #     title=f'Fields within {size} miles of school',
+            #     figsize=(10, 16)
+            # )
 
         flag_folium = False
 
