bad6b4af4b2e275d29270b0dd1f14e8787f2f57d
Nari M <nari.v.miller@gmail.com>
2024-08-27 19:37:34 -0700

    map sm edits

--- main_page.py
+++ main_page.py
@@ -49,6 +49,23 @@ selcolval = "Aircraft"
 
 dfn = filt_df(df, selcol, selcolval, type_compare="==")
 mdf, rdf = add_geometry2(dfn, fbs)
+
+
+
+
+# colormap.add_to(m)
+spriv, spub = prepare_school_pts()
+
+sz_options = np.arange(10)/2
+size  = st.select_slider('Select distance from Schools', options=sz_options)
+
+sprivb = school_buffer(spriv, size)
+spubb = school_buffer(spub, size)
+
+dfjpriv = join_buf_w_df(sprivb, mdf, howjoin = "inner", pred = "intersects")
+dfjpub = join_buf_w_df(spubb, mdf, howjoin = "inner", pred = "intersects")
+
+df = gpd.GeoDataFrame( pd.concat([dfjpriv, dfjpub], ignore_index=True), crs=dataframesList[0].crs)
 # st.write("Filter applied!")
 #################
 
@@ -70,7 +87,7 @@ m = folium.Map(location=[37.5, -120.8], zoom_start=5)
 # data = ".\pesticides\data_sources\AgComm_Stanislaus\merged_pur_sites_fieldbds.shp"
 # pur = geopandas.GeoDataFrame.from_file(data, crs="EPSG:3310")
 
-du = mdf["Product Name"].unique()
+du = df["Product Name"].unique()
 ins = [c for c in du if "insect" in c.lower()]
 herb = [c for c in du if "herbi" in c.lower()]
 fung = [c for c in du if "fungi" in c.lower()]
@@ -85,6 +102,7 @@ ccd4 = {c:ccnames[3] for c in other}
 ccd1.update(ccd2)
 ccd1.update(ccd3)
 ccd1.update(ccd4)
+dfc = df.copy()
 
 ccd2 = {ccnames[i]:ccc[i] for i in range(len(ccnames))}
 # ccd = {ccnames[i]:ccl[i] for i in ccnames}
@@ -93,7 +111,6 @@ colorcol = None
 
 
 colorcol = "Product Name"
-dfc = mdf.copy()
 dfc["color_category"] = dfc[colorcol].map(ccd1)
 dfc["color"] = dfc[colorcol].map(ccd2)
 
@@ -179,8 +196,6 @@ for _, r in dfc.iterrows():
     geo_j.add_to(m)
 # m
 
-# colormap.add_to(m)
-spriv, spub = prepare_school_pts()
 
 
 # popup = folium.GeoJsonPopup(
@@ -225,3 +240,10 @@ g2 = folium.GeoJson(
         "fillOpacity": 0.4,
     },
 ).add_to(m)
+
+
+map = st_folium(
+    m,
+    width=620, height=580,
+    key="folium_map"
+)
